f1f632686d32d142663505bb8de4ea1896b778aa
Nelson1Aguiar <132019512+Nelson1Aguiar@users.noreply.github.com>
2023-06-21 20:16:22 -0300

    Add files via upload

---
+++ projeto2_BD/src/Estudante.java
@@ -0,0 +1,58 @@
+import java.util.*;
+
+public class Estudante {
+    private int matricula;
+    private String nome;
+    private String email;
+    private String telefone;
+    private Date nascimento= new Date();
+    private boolean sexo;
+
+    public int getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(int matricula) {
+        this.matricula = matricula;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public Date getNascimento() {
+        return nascimento;
+    }
+
+    public void setNascimento(Date nascimento) {
+        this.nascimento = nascimento;
+    }
+
+    public boolean isSexo() {
+        return sexo;
+    }
+
+    public void setSexo(boolean sexo) {
+        this.sexo = sexo;
+    }
+}
